ee0985153861c9fc42a4bc392f6f7befb78fa9b0
moosaali7912002 <moosa.ali@live.com>
2026-09-16 10:24:33 +0500

    updated the incoming payload filed names

--- PostOfficeApi/Models/Dtos/PostOfficeDataRequest.cs
+++ PostOfficeApi/Models/Dtos/PostOfficeDataRequest.cs
@@ -5,57 +5,112 @@ namespace PostOfficeApi.Models.Dtos;
 public class PostOfficeDataRequest
 {
     [MaxLength(255)]
-    public string? PO_CustomerName { get; set; }
+    public string? customer_name { get; set; }
 
     [Required]
     [MaxLength(100)]
-    public string PO_TrackingNo { get; set; } = null!;
+    public string tracking_no { get; set; } = null!;
 
     [Required]
     [MaxLength(30)]
-    public string PO_MobileNo { get; set; } = null!;
+    public string mobile_no { get; set; } = null!;
 
     [EmailAddress]
     [MaxLength(255)]
-    public string? PO_EmailAddress { get; set; }
+    public string? email_address { get; set; }
 
     [Range(0, 9999999999999.999)]
-    public decimal? PO_Weight { get; set; }
+    public decimal? weight { get; set; }
 
     [MaxLength(255)]
-    public string? PO_CurrentDestination { get; set; }
+    public string? current_destination { get; set; }
 
     [MaxLength(255)]
-    public string? PO_CurrentLocation { get; set; }
+    public string? current_location { get; set; }
 
-    public DateTime? PO_CreatedAt { get; set; }
+    public DateTime? created_at { get; set; }
 
-    public DateTime? PO_MplUpdatedAt { get; set; }
-
-    [MaxLength(3)]
-    public string? PO_OriginCountryCode { get; set; }
+    public DateTime? updated_at { get; set; }
 
     [MaxLength(3)]
-    public string? PO_DestinationCountryCode { get; set; }
+    public string? origin_country { get; set; }
+
+    [MaxLength(3)]
+    public string? destination_country { get; set; }
 
     [MaxLength(1000)]
-    public string? PO_ShippingAddress { get; set; }
+    public string? shipping_address { get; set; }
 
     [MaxLength(2000)]
-    public string? PO_ItemsDescription { get; set; }
+    public string? items { get; set; }
 
     [Range(0, int.MaxValue)]
-    public int? PO_Pieces { get; set; }
+    public int? pieces { get; set; }
 
     [Range(0, 9999999999999999.99)]
-    public decimal? PO_Value { get; set; }
+    public decimal? value { get; set; }
 
     [MaxLength(100)]
-    public string? PO_PackageNumber { get; set; }
+    public string? package_number { get; set; }
 
     [MaxLength(100)]
-    public string? PO_ServiceType { get; set; }
+    public string? ServiceType { get; set; }
 
     [MaxLength(100)]
-    public string? PO_PackageLastStatus { get; set; }
+    public string? PackageLastStatus { get; set; }
+
+    //[MaxLength(255)]
+    //public string? PO_CustomerName { get; set; }
+
+    //[Required]
+    //[MaxLength(100)]
+    //public string PO_TrackingNo { get; set; } = null!;
+
+    //[Required]
+    //[MaxLength(30)]
+    //public string PO_MobileNo { get; set; } = null!;
+
+    //[EmailAddress]
+    //[MaxLength(255)]
+    //public string? PO_EmailAddress { get; set; }
+
+    //[Range(0, 9999999999999.999)]
+    //public decimal? PO_Weight { get; set; }
+
+    //[MaxLength(255)]
+    //public string? PO_CurrentDestination { get; set; }
+
+    //[MaxLength(255)]
+    //public string? PO_CurrentLocation { get; set; }
+
+    //public DateTime? PO_CreatedAt { get; set; }
+
+    //public DateTime? PO_MplUpdatedAt { get; set; }
+
+    //[MaxLength(3)]
+    //public string? PO_OriginCountryCode { get; set; }
+
+    //[MaxLength(3)]
+    //public string? PO_DestinationCountryCode { get; set; }
+
+    //[MaxLength(1000)]
+    //public string? PO_ShippingAddress { get; set; }
+
+    //[MaxLength(2000)]
+    //public string? PO_ItemsDescription { get; set; }
+
+    //[Range(0, int.MaxValue)]
+    //public int? PO_Pieces { get; set; }
+
+    //[Range(0, 9999999999999999.99)]
+    //public decimal? PO_Value { get; set; }
+
+    //[MaxLength(100)]
+    //public string? PO_PackageNumber { get; set; }
+
+    //[MaxLength(100)]
+    //public string? PO_ServiceType { get; set; }
+
+    //[MaxLength(100)]
+    //public string? PO_PackageLastStatus { get; set; }
 }
